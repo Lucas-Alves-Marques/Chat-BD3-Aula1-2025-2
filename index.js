867d58fd20c907fc6c3ea4ce718fc6fc708d5582
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-04-29 20:39:43 -0300

    Estilização do HTML e Conexão do Back com o Front

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
 const http = require('http');
-
-const app = express();
-
-const server = http.createServer(app);
-
+const socket = require('socket.io')
 const ejs = require('ejs');
 const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,6 +18,36 @@ app.engine('html', ejs.renderFile);
 app.use('/', (req, res) => {
 
     res.render('index.html')
+
+})
+
+// logica do Socket.IO - envio e propagação de mensagens
+
+//Array que simula o banco de dados:
+
+let messages = []
+
+// Estrutura de Conexão do Socket.IO
+
+io.on('connection', socketResp =>{
+    
+    console.log('Novo Usúario Conectado: ' + socketResp.id)
+
+    //Recupera e mantém (exibe) as mensagens entre o front e o back
+
+    socketResp.emit('previousMessage', messages)
+
+    //Lógica de chat quando uma mensagem é enviada
+
+    socketResp.on('sendMessage', data =>{
+
+        //Adiciona a mensagem no final do array de messagens
+
+        messages.push(data);
+
+        socketResp.broadcast.emit('receivedMessage', data)
+
+    })
 
 })
 
